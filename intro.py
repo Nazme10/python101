043c46b2c92909505a26e9d7041cb0ecd98025f8
Nazme10 <rahman10nazme@gmail.com>
2025-02-16 21:30:43 +0600

    .

---
+++ intro.py
@@ -0,0 +1,15 @@
+#output
+print("Mr.Daniel Wang")
+name = "Dan"
+print(name)
+
+#python list
+cars = ["Mercedes","GWagon"]
+print(cars)
+print(cars[-2])
+
+#conditions
+if 1>3:
+    print('3 is greater than 1')
+else:
+    print('1 is lower ')
