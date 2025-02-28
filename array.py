9ac4a1884b31873a95fb355f64260f4491c4e73a
Nazme10 <rahman10nazme@gmail.com>
2025-03-01 04:16:08 +0600

    .

---
+++ array.py
@@ -0,0 +1,23 @@
+#3-1
+names = ['Jui','Mimi','Summi','Zaheen','Ashfie']
+print(type(names))
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+
+
+#3-2
+print(f'Hello {names[0]}, How are you today?')
+print(f'Hello {names[1]}, How are you today?')
+print(f'Hello {names[2]}, How are you today?')
+print(f'Hello {names[3]}, How are you today?')
+print(f'Hello {names[4]}, How are you today?')
+
+#3-3
+cars = ['Mercedes','BMW','Bentley']
+print(f'I would like to own a {cars[0]}.')
+print(f'I would like to own a {cars[1]}.')
+print(f'I would like to own a {cars[2]}.')
+
