1e477653325b95ec64dfcb3702657ec58cf9bad0
Nazme10 <rahman10nazme@gmail.com>
2025-03-07 12:40:21 +0600

    ...

---
+++ try.py
@@ -0,0 +1,14 @@
+
+#append,insert,del,index,pop, remove
+names = ['Jui','Mimi','Summi','Zaheen','Ashfie']
+names.append('Nazme')
+print(names)
+
+names.insert(1,'Munnu')
+print(names)
+del names[3]
+#del [names.index('Nazme')]
+
+popped_val= names.pop(4)
+print(popped_val)
+names
