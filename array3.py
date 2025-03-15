6f9afeb9f3fbe9cafef1859ea3dae0af30d1725f
Nazme10 <rahman10nazme@gmail.com>
2025-03-15 11:52:34 +0600

    ...

---
+++ array3.py
@@ -0,0 +1,49 @@
+#3-8
+
+places= ['Pingxi', 'Santorini', 'Bologna', 'Jeju']
+#1
+print(places)
+print()
+#2
+print(sorted(places))
+print()
+#3
+print(places)
+print()
+
+#4
+print(sorted(places, reverse=True))
+print()
+#5
+print(places)
+print()
+
+#6
+places.reverse()
+print("In reversed order:", places)
+print()
+
+#7
+places.reverse()
+print("Again reversing to original order:", places)
+print()
+#8
+places.sort()
+print("In alphabetical order:", places)
+print()
+
+#9
+places.sort(reverse=True)
+print("In reverse alphabetical order:", places)
+print()
+print()
+print()
+#3-9
+
+people = ["Sang Yang", "Ko Gyeom", "Wei Yifan"]
+print(f"Total number of people invited: {len(people)}")
+
+
+
+
+
---
+++ loops.py
@@ -0,0 +1,4 @@
+places= ['Pingxi', 'Santorini', 'Paris', 'Jeju']
+stng ='Pingxi'
+for i in places:
+print(i)
