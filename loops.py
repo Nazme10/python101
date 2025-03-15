b99d66b32f4cd69df0c89c97097508320569ca72
Nazme10 <rahman10nazme@gmail.com>
2025-03-15 15:48:16 +0600

    ....

--- loops.py
+++ loops.py
@@ -1,4 +1,36 @@
-places= ['Pingxi', 'Santorini', 'Paris', 'Jeju']
-stng ='Pingxi'
-for i in places:
-print(i)
+
+#4-3
+for variable in range(1, 21):
+ print(variable)
+
+#4-4
+arr = list(range(1, 1000001))  
+for num in arr:
+    print(num)
+
+#4-5
+print("Min:", min(arr))
+print("Max:", max(arr))
+print("Sum:", sum(arr))
+
+#4-6
+odd_num = list(range(1, 21, 2)) 
+for num in odd_num:
+    print(num)
+#4-7
+multiples_of_3 = list(range(3, 31, 3)) 
+for num in multiples_of_3:
+    print(num)
+#4-8
+all_cube = [num**3 for num in range(1, 11)]  
+for cube in all_cube:
+    print(cube)
+#4-9
+all_cube = [num**3 for num in range(1, 11)]   
+print(all_cube) 
+
+
+
+
+
+
