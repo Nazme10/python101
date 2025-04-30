341b3d5c9aaa7af6249a4b9b91625fdd284a9150
Nazme <rahman10nazme@gmail.com>
2025-04-30 09:34:52 +0600

    ....

---
+++ 7.4 -7.7.py
@@ -0,0 +1,54 @@
+#7-4
+
+while True:
+    topping = input("Enter a pizza topping: ")
+    if topping == 'quit':
+        break
+    else:
+        print(f"I'll add {topping} to your pizza!")
+
+#7-5
+while True:
+    age_input = input("Enter your age : ")
+    if age_input == 'quit':
+        break
+    
+    age = int(age_input)
+    
+    if age < 3:
+        print("The ticket is free!")
+    elif 3 <= age <= 12:
+        print("The ticket price is $10.")
+    else:
+        print("The ticket price is $15.")
+
+#7-6
+#a)
+num=10
+while num <= 15
+  print(f"The number is {num}")
+  num +=1
+
+#b)
+variable = True
+num = 10
+
+while variable:
+    print(f"The number is {num}")
+    num +=1
+    if num > 15:
+        variable = False
+
+#c)
+hile True:
+    topping = input("Enter a pizza topping: ")
+    if topping == 'quit':
+        break
+    else:
+        print(f"I'll add {topping} to your pizza!")
+
+#7-7
+
+while True:
+
+
