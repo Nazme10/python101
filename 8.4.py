31b6840c9a7621214d01ffeedba45711bed067c9
Nazme10 <157196884+Nazme10@users.noreply.github.com>
2025-05-09 21:58:50 +0600

    .....

---
+++ 8.4.py
@@ -0,0 +1,6 @@
+#Large Shirts
+def make_shirt(size="Large", message="I love Python"):
+    print(f"The shirt size is {size} and it has the message  '{message}' written on it.")
+make_shirt()
+make_shirt("Medium")
+make_shirt("Small", "Why dont I ever have an umbrella?")
