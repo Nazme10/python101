90281e999ae6883833c4c1a2590c62fbe3eac630
Nazme10 <157196884+Nazme10@users.noreply.github.com>
2025-05-09 21:55:48 +0600

    ....

---
+++ 8.3.py
@@ -0,0 +1,8 @@
+#T-Shirt
+def make_shirt(size, message):
+    print(f"The shirt size is {size} and it has the message  '{message}' written on it.")
+
+#positional arguments
+make_shirt("Small", "Let them be them, Let us be us")
+#keyword arguments
+make_shirt(size="XL", message="It rains when I dance alone in my ecstasy")
