675a951c57edafc29b4c826537aea1b03bd7d86a
Nazme10 <157196884+Nazme10@users.noreply.github.com>
2025-05-23 20:58:44 +0600

    ......

--- try111.py
+++ try111.py
@@ -2,5 +2,14 @@
 def show_messages(messages):
     for msg in messages:
         print(msg)
+def send_messages(messages,sent_messages):
+    while messages:
+        current_sms = sms.pop(0)
+        print(f"The message that is being sent right now is: {current_sms}")
+        sent_messages.append(current_sms)
+        print(sent_messages)
+    
 sms = ['hi','how are you doin?','Its raining outside']
+sent_messages = []
 show_messages(sms)
+send_messages(sms,sent_messages)
