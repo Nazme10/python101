c6dab1cfe7479ec9e974a917dffbd153cebe0661
Nazme10 <157196884+Nazme10@users.noreply.github.com>
2025-05-23 13:08:21 +0600

    .........

---
+++ Chapter-08/8.6.py
@@ -0,0 +1,9 @@
+def city_country(city,country):
+    string = f"{city,country}"
+    return string 
+city_string = city_country('Santiago','Chilie')
+print(city_string)
+city_string = city_country('Busan','South Korea')
+print(city_string)
+city_string = city_country('Santorini','Greece')
+print(city_string)
