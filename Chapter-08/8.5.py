c6d8712da651804f049e9d7b490a12a873d4de2a
Nazme10 <157196884+Nazme10@users.noreply.github.com>
2025-05-23 09:18:55 +0600

    ....

--- Chapter-08/8.5.py
+++ Chapter-08/8.5.py
@@ -2,7 +2,7 @@
 def describe_city(city, country="Iceland"):
     print(f"{city} is in {country}.")
 
-# Calling function for three cities
-describe_city("Reykjavik")           # Default country
-describe_city("Akureyri")            # Default country
-describe_city("Seoul", "South Korea") # Different country
+
+describe_city("Reykjavik")          
+describe_city("Akureyri")            
+describe_city("Seoul", "South Korea") 
